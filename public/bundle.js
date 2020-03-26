9ffef0c2e90c2a0aac5175cbadad8a06c62a9db6
midudev <miduga@gmail.com>
2020-03-26 19:19:47 +0100

    Add conditional rendering

--- public/bundle.js
+++ public/bundle.js
@@ -24,6 +24,10 @@ var app = (function () {
     function safe_not_equal(a, b) {
         return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
     }
+
+    function append(target, node) {
+        target.appendChild(node);
+    }
     function insert(target, node, anchor) {
         target.insertBefore(node, anchor || null);
     }
@@ -33,9 +37,24 @@ var app = (function () {
     function element(name) {
         return document.createElement(name);
     }
+    function text(data) {
+        return document.createTextNode(data);
+    }
+    function space() {
+        return text(' ');
+    }
+    function empty() {
+        return text('');
+    }
     function listen(node, event, handler, options) {
         node.addEventListener(event, handler, options);
         return () => node.removeEventListener(event, handler, options);
+    }
+    function attr(node, attribute, value) {
+        if (value == null)
+            node.removeAttribute(attribute);
+        else if (node.getAttribute(attribute) !== value)
+            node.setAttribute(attribute, value);
     }
     function children(element) {
         return Array.from(element.childNodes);
@@ -249,6 +268,10 @@ var app = (function () {
     function dispatch_dev(type, detail) {
         document.dispatchEvent(custom_event(type, Object.assign({ version: '3.18.1' }, detail)));
     }
+    function append_dev(target, node) {
+        dispatch_dev("SvelteDOMInsert", { target, node });
+        append(target, node);
+    }
     function insert_dev(target, node, anchor) {
         dispatch_dev("SvelteDOMInsert", { target, node, anchor });
         insert(target, node, anchor);
@@ -270,9 +293,23 @@ var app = (function () {
             dispose();
         };
     }
+    function attr_dev(node, attribute, value) {
+        attr(node, attribute, value);
+        if (value == null)
+            dispatch_dev("SvelteDOMRemoveAttribute", { node, attribute });
+        else
+            dispatch_dev("SvelteDOMSetAttribute", { node, attribute, value });
+    }
     function prop_dev(node, property, value) {
         node[property] = value;
         dispatch_dev("SvelteDOMSetProperty", { node, property, value });
+    }
+    function set_data_dev(text, data) {
+        data = '' + data;
+        if (text.data === data)
+            return;
+        dispatch_dev("SvelteDOMSetData", { node: text, data });
+        text.data = data;
     }
     class SvelteComponentDev extends SvelteComponent {
         constructor(options) {
@@ -293,32 +330,166 @@ var app = (function () {
 
     const file = "src/Input.svelte";
 
+    // (31:2) {:else}
+    function create_else_block(ctx) {
+    	let strong;
+
+    	const block = {
+    		c: function create() {
+    			strong = element("strong");
+    			strong.textContent = "No hay resultados";
+    			add_location(strong, file, 31, 4, 635);
+    		},
+    		m: function mount(target, anchor) {
+    			insert_dev(target, strong, anchor);
+    		},
+    		p: noop,
+    		d: function destroy(detaching) {
+    			if (detaching) detach_dev(strong);
+    		}
+    	};
+
+    	dispatch_dev("SvelteRegisterBlock", {
+    		block,
+    		id: create_else_block.name,
+    		type: "else",
+    		source: "(31:2) {:else}",
+    		ctx
+    	});
+
+    	return block;
+    }
+
+    // (29:2) {#if response.length > 0}
+    function create_if_block_1(ctx) {
+    	let strong;
+    	let t0;
+    	let t1_value = /*response*/ ctx[2].length + "";
+    	let t1;
+    	let t2;
+
+    	const block = {
+    		c: function create() {
+    			strong = element("strong");
+    			t0 = text("Tenemos ");
+    			t1 = text(t1_value);
+    			t2 = text(" películas");
+    			add_location(strong, file, 29, 4, 568);
+    		},
+    		m: function mount(target, anchor) {
+    			insert_dev(target, strong, anchor);
+    			append_dev(strong, t0);
+    			append_dev(strong, t1);
+    			append_dev(strong, t2);
+    		},
+    		p: function update(ctx, dirty) {
+    			if (dirty & /*response*/ 4 && t1_value !== (t1_value = /*response*/ ctx[2].length + "")) set_data_dev(t1, t1_value);
+    		},
+    		d: function destroy(detaching) {
+    			if (detaching) detach_dev(strong);
+    		}
+    	};
+
+    	dispatch_dev("SvelteRegisterBlock", {
+    		block,
+    		id: create_if_block_1.name,
+    		type: "if",
+    		source: "(29:2) {#if response.length > 0}",
+    		ctx
+    	});
+
+    	return block;
+    }
+
+    // (26:0) {#if loading}
+    function create_if_block(ctx) {
+    	let strong;
+
+    	const block = {
+    		c: function create() {
+    			strong = element("strong");
+    			strong.textContent = "Loading...";
+    			add_location(strong, file, 26, 2, 500);
+    		},
+    		m: function mount(target, anchor) {
+    			insert_dev(target, strong, anchor);
+    		},
+    		p: noop,
+    		d: function destroy(detaching) {
+    			if (detaching) detach_dev(strong);
+    		}
+    	};
+
+    	dispatch_dev("SvelteRegisterBlock", {
+    		block,
+    		id: create_if_block.name,
+    		type: "if",
+    		source: "(26:0) {#if loading}",
+    		ctx
+    	});
+
+    	return block;
+    }
+
     function create_fragment(ctx) {
     	let input;
+    	let t;
+    	let if_block_anchor;
     	let dispose;
+
+    	function select_block_type(ctx, dirty) {
+    		if (/*loading*/ ctx[1]) return create_if_block;
+    		if (/*response*/ ctx[2].length > 0) return create_if_block_1;
+    		return create_else_block;
+    	}
+
+    	let current_block_type = select_block_type(ctx);
+    	let if_block = current_block_type(ctx);
 
     	const block = {
     		c: function create() {
     			input = element("input");
+    			t = space();
+    			if_block.c();
+    			if_block_anchor = empty();
+    			attr_dev(input, "placeholder", "Search movies...");
     			input.value = /*value*/ ctx[0];
-    			add_location(input, file, 16, 0, 328);
+    			add_location(input, file, 19, 0, 399);
     		},
     		l: function claim(nodes) {
     			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, input, anchor);
-    			dispose = listen_dev(input, "input", /*handleInput*/ ctx[1], false, false, false);
+    			insert_dev(target, t, anchor);
+    			if_block.m(target, anchor);
+    			insert_dev(target, if_block_anchor, anchor);
+    			dispose = listen_dev(input, "input", /*handleInput*/ ctx[3], false, false, false);
     		},
     		p: function update(ctx, [dirty]) {
     			if (dirty & /*value*/ 1 && input.value !== /*value*/ ctx[0]) {
     				prop_dev(input, "value", /*value*/ ctx[0]);
+    			}
+
+    			if (current_block_type === (current_block_type = select_block_type(ctx)) && if_block) {
+    				if_block.p(ctx, dirty);
+    			} else {
+    				if_block.d(1);
+    				if_block = current_block_type(ctx);
+
+    				if (if_block) {
+    					if_block.c();
+    					if_block.m(if_block_anchor.parentNode, if_block_anchor);
+    				}
     			}
     		},
     		i: noop,
     		o: noop,
     		d: function destroy(detaching) {
     			if (detaching) detach_dev(input);
+    			if (detaching) detach_dev(t);
+    			if_block.d(detaching);
+    			if (detaching) detach_dev(if_block_anchor);
     			dispose();
     		}
     	};
@@ -336,6 +507,7 @@ var app = (function () {
 
     function instance($$self, $$props, $$invalidate) {
     	let value = "";
+    	let loading = false;
     	let response = [];
     	const handleInput = event => $$invalidate(0, value = event.target.value);
 
@@ -345,20 +517,24 @@ var app = (function () {
 
     	$$self.$inject_state = $$props => {
     		if ("value" in $$props) $$invalidate(0, value = $$props.value);
-    		if ("response" in $$props) response = $$props.response;
+    		if ("loading" in $$props) $$invalidate(1, loading = $$props.loading);
+    		if ("response" in $$props) $$invalidate(2, response = $$props.response);
     	};
 
     	$$self.$$.update = () => {
     		if ($$self.$$.dirty & /*value*/ 1) {
     			 if (value.length > 2) {
+    				$$invalidate(1, loading = true);
+
     				fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`).then(res => res.json()).then(apiResponse => {
-    					response = apiResponse.Search;
+    					$$invalidate(2, response = apiResponse.Search || []);
+    					$$invalidate(1, loading = false);
     				});
     			}
     		}
     	};
 
-    	return [value, handleInput];
+    	return [value, loading, response, handleInput];
     }
 
     class Input extends SvelteComponentDev {
--- src/Input.svelte
+++ src/Input.svelte
@@ -1,17 +1,34 @@
 <script>
   let value = ''
+  let loading = false
   let response = []
 
   const handleInput = (event) =>
     value = event.target.value
 
   $: if (value.length > 2) {
+    loading = true
     fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
       .then(res => res.json())
       .then(apiResponse => {
-        response = apiResponse.Search
+        response = apiResponse.Search || []
+        loading = false
       })
   }
 </script>
 
-<input value={value} on:input={handleInput} />
+<input
+  placeholder="Search movies..."
+  value={value}
+  on:input={handleInput}
+/>
+
+{#if loading}
+  <strong>Loading...</strong>
+{:else}
+  {#if response.length > 0}
+    <strong>Tenemos {response.length} películas</strong>
+  {:else}
+    <strong>No hay resultados</strong>
+  {/if}
+{/if}
