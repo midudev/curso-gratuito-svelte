0bd71caad9445eb20f5cbc6aa71649fc2cc17f34
midudev <miduga@gmail.com>
2020-04-13 20:12:55 +0200

    Add new class content

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Movie from './Movie.svelte'
+
   let value = ''
   let loading = false
   let response = []
@@ -26,9 +28,14 @@
 {#if loading}
   <strong>Loading...</strong>
 {:else}
-  {#if response.length > 0}
-    <strong>Tenemos {response.length} películas</strong>
+  {#each response as {Title, Poster, Year}, index}
+    <Movie
+      index={index}
+      title={Title}
+      poster={Poster}
+      year={Year}
+    />
   {:else}
     <strong>No hay resultados</strong>
-  {/if}
+  {/each}
 {/if}
---
+++ src/Movie.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let title, poster, year
+</script>
+
+<style>
+article {
+  border: 1px solid #eee;
+  border-radius: 4px;
+  margin-bottom: 8px;
+  padding: 16px;
+}
+
+h3 {
+  font-size: 24px;
+  margin-bottom: 4px;
+}
+</style>
+
+<article>
+  <img alt={title} src={poster} />
+  <h3>{title}</h3>
+  <span>{year}</span>
+  {#if 2020 - year < 5}
+    <i>🆕</i>
+  {/if}
+</article>
